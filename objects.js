cb8f89805849e55915dfdcdaec903ec1090b134a
Gabriela <gabrielaa.bdasilva@gmail.com>
2022-09-24 17:30:39 -0700

    Completed objects

--- objects.js
+++ objects.js
@@ -2,10 +2,32 @@
 
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
+  const wordCount = {};
+
+  for (const word of phrase.split(" ")) {
+    if (wordCount[word]) {
+      wordCount[word] += 1;
+    }
+    else {
+      wordCount[word] = 1;
+    }
+  }
+  return wordCount;
 }
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
-  // Replace this with your code
+  const melonPrices = {
+    2.50: ['Cantaloupe', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas']
+  };
+
+  if (!melonPrices[price]) {
+    return null;
+  }
+
+  return melonPrices[price].sort();
+
 }
