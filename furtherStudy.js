ef2d8280de2c6b3ae87b7b05db0ac7001cf34134
Gabriela <gabrielaa.bdasilva@gmail.com>
2022-09-25 11:28:26 -0700

    Completed arrays and functions

--- furtherStudy.js
+++ furtherStudy.js
@@ -1,7 +1,19 @@
 'use strict';
 
 function wordsInCommon(words1, words2) {
-  // Replace this with your code
+  const wordSet1 = new Set(words2);
+  const wordSet2 = new Set(words2);
+
+  const result = new Set();
+
+  for (const word of wordSet1) {
+
+    if (wordSet2.has(word)) {
+      result.add(word);
+    }
+  }
+  return Array.from(result);
+
 }
 
 function kidsGame(names) {
